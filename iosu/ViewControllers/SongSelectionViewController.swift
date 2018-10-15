b0217b08257796aef0453655cba327172f519495
Alexey Bodnya <fermerasb@gmail.com>
2018-10-15 09:17:54 +0300

    add difficulty selection

--- iosu/ViewControllers/SongSelectionViewController.swift
+++ iosu/ViewControllers/SongSelectionViewController.swift
@@ -11,6 +11,18 @@ import CoreData
 
 class SongSelectionViewController: UIViewController {
     
+    enum ItemCell {
+        case song(SongInfo)
+        case file(BeatmapFile)
+        
+        var songInfo: SongInfo {
+            switch self {
+            case .song(let song): return song
+            case .file(let file): return file.song!
+            }
+        }
+    }
+    
     @IBOutlet weak var backgroundImageView: UIImageView!
     
     @IBOutlet weak var tableView: UITableView!
@@ -18,6 +30,7 @@ class SongSelectionViewController: UIViewController {
     private let player = MusicPlayer()
     
     var selectedSongName = ""
+    var selectedFileName = ""
     
     let resultsController: NSFetchedResultsController<SongInfo> = {
         let request: NSFetchRequest<SongInfo> = SongInfo.fetchRequest()
@@ -26,6 +39,8 @@ class SongSelectionViewController: UIViewController {
         let controller = NSFetchedResultsController<SongInfo>(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         return controller
     }()
+    
+    var items = [ItemCell]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,22 +74,15 @@ class SongSelectionViewController: UIViewController {
     
     @objc func databaseLoaded() {
         try! resultsController.performFetch()
-        if let first = resultsController.fetchedObjects!.first {
-            selectedSongName = first.folderName ?? ""
-            backgroundImageView.image = first.backgroundImage
-            startPlayingMusic(first)
+        items = resultsController.fetchedObjects!.map { .song($0) }
+        if resultsController.fetchedObjects!.count > 0 {
+            selectSong(at: 0)
         }
         tableView.reloadData()
     }
     
-    func startGameForSong(_ song: SongInfo) {
-        guard let files = song.files?.map({ $0 as! BeatmapFile }), files.count > 0 else {
-            return
-        }
-        let file = files.sorted { (file1, file2) -> Bool in
-            return file1.difficulty < file2.difficulty
-        }.first!
-        let viewController = GameViewController.create(songInfo: song, beatmapFile: file)
+    func startGame(for file: BeatmapFile) {
+        let viewController = GameViewController.create(songInfo: file.song!, beatmapFile: file)
         navigationController?.pushViewController(viewController, animated: false)
     }
     
@@ -83,6 +91,60 @@ class SongSelectionViewController: UIViewController {
             player.loadFile(url.path)
             player.rewind(at: 30.0)
             player.play()
+        }
+    }
+    
+    func selectSong(at index: Int) {
+        let item = items[index]
+        switch item {
+        case .song(let selectedSong):
+            if selectedSongName != "", let previousSongInfo = resultsController.fetchedObjects!.first(where: { $0.folderName == selectedSongName }) {
+                // hide previous selected
+                let indexes = items.enumerated()
+                    .filter { (index, item) -> Bool in
+                        if case .file = item {
+                            return true
+                        }
+                        return false
+                    }.map {
+                        $0.offset
+                    }
+                if indexes.count > 0 {
+                    let songItem: ItemCell = .song(previousSongInfo)
+                    items.replaceSubrange(indexes.first! ... indexes.last!, with: [songItem])
+                }
+            }
+            let insertIndex = items.firstIndex { (item) -> Bool in
+                if case .song(let song) = item {
+                    return song.folderName! == selectedSong.folderName!
+                }
+                return false
+            }!
+            let insertedFiles = (selectedSong.files ?? NSSet())
+                .map { $0 as! BeatmapFile}
+                .sorted { (file1, file2) -> Bool in
+                    return file1.difficulty < file2.difficulty
+                }
+            let insertedItems = insertedFiles
+                .map { (file) -> ItemCell in
+                    return .file(file)
+                }
+            selectedFileName = insertedFiles.first?.fileName ?? ""
+            items.replaceSubrange(insertIndex ... insertIndex, with: insertedItems)
+            selectedSongName = selectedSong.folderName ?? ""
+            backgroundImageView.image = selectedSong.backgroundImage
+            startPlayingMusic(selectedSong)
+            SoundPlayer.instance.playSound(.menuClick)
+            tableView.reloadData()
+        case .file(let file):
+            if file.fileName == selectedFileName {
+                SoundPlayer.instance.playSound(.menuHit)
+                startGame(for: file)
+            } else {
+                selectedFileName = file.fileName ?? ""
+                SoundPlayer.instance.playSound(.menuClick)
+                tableView.reloadData()
+            }
         }
     }
 }
@@ -98,37 +160,35 @@ extension SongSelectionViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
-        let songInfo = resultsController.fetchedObjects![indexPath.row]
-        if selectedSongName == songInfo.folderName {
-            // second selection
-            SoundPlayer.instance.playSound(.menuHit)
-            startGameForSong(songInfo)
-        } else {
-            selectedSongName = songInfo.folderName ?? ""
-            backgroundImageView.image = songInfo.backgroundImage
-            startPlayingMusic(songInfo)
-            SoundPlayer.instance.playSound(.menuClick)
-            
-            tableView.reloadData()
-        }
+        selectSong(at: indexPath.row)
     }
 }
 
 extension SongSelectionViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return resultsController.fetchedObjects?.count ?? 0
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SongSelectionCell
-        let songInfo = resultsController.fetchedObjects![indexPath.row]
+        let item = items[indexPath.row]
+        let songInfo = item.songInfo
         cell.backgroundImageView.image = songInfo.backgroundImage
-        cell.backgroundColorView.backgroundColor = selectedSongName == songInfo.folderName ? .blue : songInfo.backgroundColor
-        cell.nameLabel.textColor = songInfo.primaryColor
+        var textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        switch item {
+        case .song:
+            cell.backgroundColorView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            cell.difficultyLabel.text = nil
+        case .file(let file):
+            cell.backgroundColorView.backgroundColor = selectedFileName == file.fileName ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+            textColor = selectedFileName == file.fileName ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            cell.difficultyLabel.text = "Difficulty: \(file.difficulty)"
+        }
+        
+        cell.nameLabel.textColor = textColor
         cell.nameLabel.text = songInfo.name
-        cell.difficultyLabel.textColor = songInfo.secondaryColor
-        cell.difficultyLabel.text = nil
+        cell.difficultyLabel.textColor = textColor
         cell.rateViewContainer.isHidden = true
         return cell
     }
